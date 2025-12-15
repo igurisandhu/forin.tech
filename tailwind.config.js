8d99cda5d06601c894cef8a04883216923832183
inavdeep13 <inavdeep13@gmail.com>
2025-12-16 00:10:05 +0530

    frontend changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
